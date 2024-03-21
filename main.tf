c90898df1e91e862b7a99bde16d675b851281b79
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-03-21 22:36:37 +0200

    ci: remove cloud block in main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,4 @@
 terraform {
-  # replace var.tfc_organization & var.tfc_workspace_name with their string values in CLI driven workflow
-  cloud {
-    organization = var.tfc_organization
-
-    workspaces {
-      name = var.tfc_workspace_name
-    }
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
