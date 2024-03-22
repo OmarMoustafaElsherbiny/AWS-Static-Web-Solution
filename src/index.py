d1a30d1579d114c2676d4f28816fef9498d188a2
Omar Mostafa <omar.mostafa.email@gmail.com>
2024-03-22 16:36:49 +0200

    fix(lambda): change the Id value from string to Number
    Having value as string instead of number caused the "The provided key element does not match the schema" Internal server error or 502 Bad Gateway which prevented lambda from returning the response to the client/frontend

--- src/index.py
+++ src/index.py
@@ -4,13 +4,13 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('site-vistors')
 def lambda_handler(event, context):
     response = table.get_item(Key={
-        'Id':'1'
+        'Id': 1
     })
     views = response['Item']['Views']
     views = views + 1
     # print(views)
     response = table.put_item(Item={
-        'Id':'1',
+        'Id': 1,
         'Views':views
     })
     
